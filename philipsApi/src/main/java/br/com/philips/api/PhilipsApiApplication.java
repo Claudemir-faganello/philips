75070634853fe7077c6a2ad1a748eff4a87c4069
c-faganello <=>
2021-09-13 23:41:58 -0300

    Api: cria classe para start da Api

---
+++ philipsApi/src/main/java/br/com/philips/api/PhilipsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.philips.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhilipsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhilipsApiApplication.class, args);
+	}
+
+}
